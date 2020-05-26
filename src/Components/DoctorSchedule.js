aa5131dad7beecedc76edb0305180e94f9c21067
mbessalle <mbessalle@gmail.com>
2020-05-26 13:02:22 +0200

    Modified Doctor Scheduler

--- src/Components/DoctorSchedule.js
+++ src/Components/DoctorSchedule.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function DoctorSchedule() {
   const date = new Date();
   const time = date.getHours();
-  
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [doctors, setDoctors] = useState([]);
+
   return (
     <div>
       <p>we are: {time >= 8 && time <= 17 ? "open" : "closed"}</p>
